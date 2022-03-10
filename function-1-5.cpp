b1b009293869d65d2f817ac252468793a1859786
Yifuhuang <100665305+Yifuhuang@users.noreply.github.com>
2022-03-10 17:13:54 +0800

    Update function-1-5.cpp

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -15,10 +15,12 @@ array1[i][j] += array2[i][j];
 
 for(int i=0; i<3; i++){
 for(int j=0; j<3; j++){
+if(j<2){
 std::cout<<array1[i][j]<<" ";
+}else{
+   std::cout<<array1[i][j] 
+}
 }
 std::cout<<std::endl;
 }
 }
-
-
