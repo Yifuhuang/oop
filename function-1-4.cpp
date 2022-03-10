e0d2fefbe3a3624de024f42b7265def7257d6210
Yifuhuang <100665305+Yifuhuang@users.noreply.github.com>
2022-03-10 17:08:55 +0800

    Update function-1-4.cpp

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <stdlib.h>
 void print_scaled_matrix(int array[3][3], int scale){
-//
+
 for(int i=0; i<3; i++){
 
 
@@ -14,7 +14,11 @@ array[i][j] *= scale;
 
 for(int i=0; i<3; i++){
 for(int j=0; j<3; j++){
+if(j<2){
 std::cout<<array[i][j]<<" ";
+}else{
+   std::cout<<array[i][j] ;
+}
 }
 std::cout<<std::endl;
 }
