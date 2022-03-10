0ebe23cc7e7278dfd700132be1d314fc7198d5ee
Yifuhuang <100665305+Yifuhuang@users.noreply.github.com>
2022-03-10 16:43:56 +0800

    Add files via upload

---
+++ function-1-4.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <stdlib.h>
+void print_scaled_matrix(int array[3][3], int scale){
+
+for(int i=0; i<3; i++){
+
+
+for(int j=0; j<3; j++){
+array[i][j] *= 3;
+}
+
+}
+
+for(int i=0; i<3; i++){
+for(int j=0; j<3; j++){
+std::cout<<array[i][j]<<" ";
+}
+std::cout<<std::endl;
+}
+}
