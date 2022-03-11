dfde6118fd1f139c44ff777c17c965bf8f9e09d7
Yifuhuang <100665305+Yifuhuang@users.noreply.github.com>
2022-03-11 19:05:58 +0800

    Update main-2-4.cpp

--- main-2-4.cpp
+++ main-2-4.cpp
@@ -1,10 +1,10 @@
 
 #include <iostream>
 #include <stdlib.h>
-extern int sum_min_max(int*,int);
+extern int sum_min_and_max(int*,int);
 int main(int argc,char **argv)
 {
 int test_array[5] = {2,7,9,11,10};
-    std::cout << sum_min_max(test_array, 5) << std::endl;
+    std::cout << sum_min_and_max(test_array, 5) << std::endl;
     return 0;
 }
