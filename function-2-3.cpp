1b13e6475f8b06f9f4d69a79e3fb915a78a63482
Yifuhuang <100665305+Yifuhuang@users.noreply.github.com>
2022-03-11 18:49:12 +0800

    Update function-2-3.cpp

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -2,21 +2,21 @@
  
  
 
-int sum_elements(int arr[],int n){
+int sum_elements(int array[],int n){
 
 int sum=0;
 
 for(int a=0;a<n;a++){
-    sum+=arr[a];
+    sum+=array[a];
 }
 return sum;
 
 }
  
- bool is_a_palindrome(int arr[],int n){
+ bool is_a_palindrome(int array[],int n){
 int result=1;
 for(int a=0; a<n;a++){
-if(arr[a]!=arr[n-1-a]){
+if(array[a]!=array[n-1-a]){
     result=0;
 }
 
@@ -29,14 +29,14 @@ if(result==0){
 }
  
  
- int sum_if_a_palindrome(int arr[],int n){
+ int sum_if_a_palindrome(int array[],int n){
 int result = -2;
 if(n<1){
     return -1;
 }
-if(is_a_palindrome(arr,n)){
+if(is_a_palindrome(array,n)){
 
-return sum_elements(arr,n);
+return sum_elements(array,n);
 
 }else{ 
     return result;
