0810c4df4705b9e28194fa56d238404ddb98f4f8
Yifuhuang <100665305+Yifuhuang@users.noreply.github.com>
2022-03-18 13:44:00 +0800

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-
+#include <stdio.h>
 void printer(int nums[10][10]) {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-             if (j == 9) {
-              std::cout<<nums[i][j];
+    for (int a = 0; a < 10; a++) {
+        for (int b = 0; b < 10; b++) {
+             if (b == 9) {
+              std::cout<<nums[a][b];
               std::cout << std::endl;
           }
-          else if(j>=0&&j<9){
-              std::cout<<nums[i][j];
+          else if(b>=0&&b<9){
+              std::cout<<nums[a][b];
               std::cout<<" ";
           }
        }
