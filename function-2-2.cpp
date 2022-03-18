eb5235a197df95c0725a2c3eb9a6209759dd4832
Yifuhuang <100665305+Yifuhuang@users.noreply.github.com>
2022-03-18 13:25:44 +0800

    Update function-2-2.cpp

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <stdio.h>
 int binary_to_number(int binary[], int n) {
-    int ten = 0,a=0;
+    int ten = 0,a=0,i=(n-1);
 while(a<n){
     ten=ten*2;
-    ten+=binary[a];
+    ten=ten+binary[a];
     a++;
 
     }
