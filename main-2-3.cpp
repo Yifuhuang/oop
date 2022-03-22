6d6e2081dce167df67f34eb1b2e60aeb4c8af02e
Yifuhuang <100665305+Yifuhuang@users.noreply.github.com>
2022-03-22 10:51:33 +0800

    Delete main-2-3.cpp

--- main-2-3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdlib.h>
-extern int sum_if_a_palindrome(int*,int);
-int main(int argc,char **argv)
-{
-int test_array[5] = {1,1,2,1,0};
-    std::cout << sum_if_a_palindrome(test_array, 5) << std::endl;
-    return 0;
-}
