a8857e3db494f74c4ef4e0c22244c7dcef0a752e
Yifuhuang <100665305+Yifuhuang@users.noreply.github.com>
2022-04-01 13:56:24 +0800

    Add files via upload

---
+++ function-4-1.cpp
@@ -0,0 +1,1 @@
+#include "function-4-1.h"
---
+++ main-4-1.cpp
@@ -0,0 +1,19 @@
+#include<string>
+#include<iostream>
+#include "function-4-1.h"
+
+using namespace std;
+
+int main(){
+  // set up room with capacity of 3 people
+  // add some people
+  for(int i=0;i<4;i++){
+    cout << "num people: " << i << endl;
+    
+  }
+  // exit some people 
+  for(int i=3;i>-1;i--){
+    cout << "num people: " << i << endl;
+    
+  }
+}
